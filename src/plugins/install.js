076674cc06e981a6d33335e7201f4529ac0e3aca
weilan <17796646068@163.com>
2020-07-07 17:59:04 +0800

    fix: v0.1.4 修复下拉框部分问题，更换bim loading图标

--- src/plugins/install.js
+++ src/plugins/install.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import ElementUi from "./element"
-// import WeilanUi from "./wlui"
-import WeilanUi from "weilan-ui"
-import "weilan-ui/weilan-ui.css"
+import WeilanUi from "./wlui"
+// import WeilanUi from "weilan-ui"
+// import "weilan-ui/weilan-ui.css"
 ElementUi.concat(WeilanUi).forEach(i => Vue.use(i))
